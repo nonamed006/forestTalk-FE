30363024fb2eb3e9aa716aca3b764a675a6ff607
firstghrn <firstghrn@naver.com>
2023-08-03 19:40:16 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import './App.css';
-import TestCom from './component/TestComp';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import TestCom from "./component/TestComp";
 
 function App() {
-  return (<div className='root'>
-    {/* <Router>
+  return (
+    <div className="root">
+      {/* <Router>
         <Routes>
             <Route path="/testPage" element={<TestCom />} />
             <Route path='/*' element={<h2><br/>잘못된 접근입니다.</h2>} />
         </Routes>
       </Router> */}
-            <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<TestCom />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>);
+    </div>
+  );
 }
 
 export default App;
