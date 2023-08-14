193693322190654b20e5f1b0567f5ac246a5c5eb
PUO <86181292+kl204@users.noreply.github.com>
2023-08-14 22:11:14 +0900

    update UserBoard.js
    Change data in table

--- src/component/UserBoard.js
+++ src/component/UserBoard.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 import { Space, Table, Tag } from 'antd';
 import { PORT } from '../set';
 
+/*
+ 회원 게시판 화면
+ 김선규
+*/
+
 const columns = [
     {
       title: 'Name',
@@ -52,32 +57,19 @@ const columns = [
     },
   ];
 
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
-
 export default function UserBoard() {
-    const [boardData, setBoardData] = useState([]);
+    // const [boardData, setBoardData] = useState([]);/
+    const [boardDataSec, setBoardDataSec] = useState([]);
+
+    let dataSec = [
+      {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+      },
+    ];
 
 	useEffect(() => {
 		fetch(`${PORT}/test/userBoard`, {
@@ -85,15 +77,32 @@ export default function UserBoard() {
 		})
         .then((res) => res.json())  // 데이터를 텍스트로 추출
         .then((data) => {
-            setBoardData(data.data);  // 데이터를 상태에 설정
-            console.log(data.data);
+            const boardData = data.data;  // 데이터를 상태에 설정, 첫번째 data는 response의 data, 두번째 data는 Spring ApiResult 클래스의 List 이름이 data
+
+            console.log(boardData);
+
+            const updatedDataSec = dataSec.map((boardItem, index) => {
+
+              console.log(boardData[index].test);
+
+              return {
+                key: index,
+                name: '로시',
+                age: 1, // 예시로 고정된 값을 사용
+                address: boardData[index].test, // 예시로 고정된 값을 사용
+                tags: ['강아지', '고양이'], // 예시로 고정된 값을 사용
+              };
+            });
+
+            setBoardDataSec(updatedDataSec);
+
         });
 	}, []);
 
     return (
         <div>
 
-            <Table columns={columns} dataSource={data}/> 
+            <Table columns={columns} dataSource={boardDataSec}/> 
 
             {/* <Table>
                 <Table.Header>
