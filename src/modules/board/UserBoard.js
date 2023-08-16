d3502b80bf3ba3b80b91f98cef640155af374b99
DESKTOP-1IRTIK9\금정산2_PC22 <>
2023-08-16 14:09:59 +0900

    UPDATE UserBoard.js , ADD UserBoard.css

--- src/modules/board/UserBoard.js
+++ src/modules/board/UserBoard.js
@@ -1,152 +1,152 @@
-
 import { useEffect, useState } from "react";
-import { Space, Table, Tag } from 'antd';
-import { PORT } from "../../set";
+import { Space, Table, Tag, Input, Select, Button } from 'antd';
+import { PORT } from '../../set';
+import './UserBoard.css';
+
 
 /*
  회원 게시판 화면
  김선규
 */
-
 const columns = [
-    {
-      title: '순번',
-      dataIndex: 'bSeq',
-      key: 'bSeq',
-      width: '10%',
-    },
-    {
-      title: '작성자',
-      dataIndex: 'uSeq',
-      key: 'uSeq',
-    },
-    {
-      title: '제목',
-      dataIndex: 'bTitle',
-      key: 'bTitle',
-      width: '40%',
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: '조회수',
-      dataIndex: 'bCount',
-      key: 'bCount',
-    },
-    {
-      title: '작성일',
-      dataIndex: 'CDT',
-      key: 'CDT',
-    },
+  {
+    title: '순번',
+    dataIndex: 'bSeq',
+    key: 'bSeq',
+    width: '10%',
+  },
+  {
+    title: '작성자',
+    dataIndex: 'uSeq',
+    key: 'uSeq',
+  },
+  {
+    title: '제목',
+    dataIndex: 'bTitle',
+    key: 'bTitle',
+    width: '40%',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: '조회수',
+    dataIndex: 'bCount',
+    key: 'bCount',
+  },
+  {
+    title: '작성일',
+    dataIndex: 'CDT',
+    key: 'CDT',
+  },
+];
 
-    //<나중에 사용할 태그 형식>
-    // {
-    //   title: 'Tags',
-    //   key: 'tags',
-    //   dataIndex: 'tags',
-    //   render: (_, { tags }) => (
-    //     <>
-    //       {tags.map((tag) => {
-    //         let color = tag.length > 5 ? 'geekblue' : 'green';
-    //         if (tag === 'loser') {
-    //           color = 'volcano';
-    //         }
-    //         return (
-    //           <Tag color={color} key={tag}>
-    //             {tag.toUpperCase()}
-    //           </Tag>
-    //         );
-    //       })}
-    //     </>
-    //   ),
-    // },
+//검색 누르면 검색 텍스트 콘솔에 띄우기
+const { Search } = Input;
+const onSearch = (value) => console.log(value);
 
-    //<나중에 사용할 Space 형식>
-    // {
-    //   title: 'Action',
-    //   key: 'action',
-    //   render: (_, record) => (
-    //     <Space size="middle">
-    //       <a>Invite {record.name}</a>
-    //       <a>Delete</a>
-    //     </Space>
-    //   ),
-    // },
-  ];
+//셀렉트 박스 체인지 확인
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+
+//글 작성하기
+const registBoard = () =>{
+  
+}
+
 
 export default function UserBoard() {
-    // const [boardData, setBoardData] = useState([]);/
-    const [boardDataSec, setBoardDataSec] = useState([]);
+  // const [boardData, setBoardData] = useState([]);/
+  const [boardDataSec, setBoardDataSec] = useState([]);
 
-    let dataSec = [
-      {
-        key: 0,
-        bSeq: 0,
-        uSeq: 0,
-        bTitle: '',
-        bCount: 0,
-        CDT: '',
-    
-        // tags: ['nice', 'developer'],
-      },
-    ];
+  useEffect(() => {
+    fetch(`${PORT}/userBoard/selectAllBoard`, {
+      method: "get",
+    })
+      .then((res) => res.json())  // 데이터를 텍스트로 추출
+      .then((data) => {
+        const boardData = data.data;  // 데이터를 상태에 설정, 첫번째 data는 response의 data, 두번째 data는 Spring ApiResult 클래스의 List 이름이 data
 
-	useEffect(() => {
-		fetch(`${PORT}/test/userBoard`, {
-			method: "get",
-		})
-        .then((res) => res.json())  // 데이터를 텍스트로 추출
-        .then((data) => {
-            const boardData = data.data;  // 데이터를 상태에 설정, 첫번째 data는 response의 data, 두번째 data는 Spring ApiResult 클래스의 List 이름이 data
+        console.log("데이터 리스트" + boardData);
 
-            console.log(boardData);
+        const updatedDataSec = boardData.map((boardItem, index) => {
 
-            const updatedDataSec = dataSec.map((boardItem, index) => {
+          return {
+            key: index,
+            bSeq: boardItem.bseq,
+            uSeq: boardItem.useq,
+            bTitle: boardItem.btitle,
+            bCount: boardItem.bcount,
+            CDT: boardItem.cdt,
+          };
 
-              console.log(boardData[index].bseq);
-
-              return {
-                key: index,
-                bSeq: boardData[index].bseq,
-                uSeq: boardData[index].useq,
-                bTitle: boardData[index].btitle,
-                bCount: boardData[index].bcount,
-                CDT: boardData[index].cdt,
-                
-                // tags: ['강아지', '고양이'], 
-              };
-            });
-
-            setBoardDataSec(updatedDataSec);
 
         });
-	}, []);
 
-    return (
-        <div>
+        setBoardDataSec(updatedDataSec);
 
-            <Table columns={columns} dataSource={boardDataSec}/> 
+      });
+  }, []);
 
-            {/* <Table>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Test</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
+  const [pageSize, setPageSize] = useState(5);
+  const [bottom, setBottom] = useState('bottomCenter');
 
-                <Table.Body>
-                {boardData.map((boardItem, index) => (
-                        <Table.Row key={index}>
-                            <Table.Cell>{boardItem.cdt}</Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
+  return (
 
-            </Table> */}
+    <div>
+      <ul style={{ listStyleType: 'none' }}>
+        <li>
+          <h1>회원 게시판 </h1>
+        </li>
+        <li className="registButton">
+          <Button onClick={registBoard}>글 작성하기</Button>
+        </li>
+        <li>
+          <Table
+            columns={columns}
+            dataSource={boardDataSec}
+            pagination={{
+              position: [bottom]
+              , pageSize: [pageSize]
+              ,
+            }} />
+        </li>
+        <li className="liStyles">
+          <Space wrap>
+            <Select
+              defaultValue="전체"
+              style={{
+                width: 80,
+              }}
+              onChange={handleChange}
+              options={[
+                {
+                  value: 'searchTitleText',
+                  label: '제목',
+                },
+                {
+                  value: 'searchWriterText',
+                  label: '작성자',
+                },
 
-        </div>
+              ]}
+            />
+
+            <Search
+              placeholder="뭐"
+              allowClear
+              enterButton="검색"
+              size="large"
+              onSearch={onSearch}
+              className="suffix"
+            />
+
+          </Space>
+        </li>
+      </ul>
+    </div>
 
 
 
-    )
+  )
 
 }
--- src/modules/route/routeInfo.js
+++ src/modules/route/routeInfo.js
@@ -1,9 +1,11 @@
 import FileBoard from "../board/FileBoard";
+import UserBoard from "../board/UserBoard";
 import LoginForm from "../login/LoginForm";
 import LoginTest from "../login/LoginTest";
 
 export const pathInfo = [
 	{ board: ['all-board', 'LoginTest', 'List', 'fileBoard'] },
+	{ board: ['all-board', 'LoginTest', 'List' ,'userboard'] },
 	{ main: ['main1', 'main2'] }
 ]
 
@@ -17,6 +19,9 @@ export const routeInfo = (path) => {
 			return <LoginTest />
 		case '/board/fileBoard' :
 			return <FileBoard />
+		case '/board/userboard':
+			console.log('path', path);
+			return <UserBoard />
 		default:
 			return <div style={{backgroundColor: 'white',}}></div>
 
