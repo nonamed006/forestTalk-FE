b9d3a0cc41d10cb6b1baf02957889cee8b5cdd73
eunbinpark <534311@gmail.com>
2023-08-16 13:26:16 +0900

    230816 자료 게시판 테이블 만드는 중

--- src/modules/route/routeInfo.js
+++ src/modules/route/routeInfo.js
@@ -1,8 +1,9 @@
+import FileBoard from "../board/FileBoard";
 import LoginForm from "../login/LoginForm";
 import LoginTest from "../login/LoginTest";
 
 export const pathInfo = [
-	{ board: ['all-board', 'LoginTest', 'List'] },
+	{ board: ['all-board', 'LoginTest', 'List', 'fileBoard'] },
 	{ main: ['main1', 'main2'] }
 ]
 
@@ -14,6 +15,8 @@ export const routeInfo = (path) => {
 		case '/board/LoginTest':
 			console.log('path', path);
 			return <LoginTest />
+		case '/board/fileBoard' :
+			return <FileBoard />
 		default:
 			return <div style={{backgroundColor: 'white',}}></div>
 
